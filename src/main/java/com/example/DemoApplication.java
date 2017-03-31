aa5d987467da3acdc34f20ae641a06c86dee3f23
fel <fxkuntz@gmail.com>
2017-03-31 23:04:01 +0200

    env/profile based configuration for the masses

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,15 +1,20 @@
 package com.example;
 
 import fr.weit.epub.DecoratedBook;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 
 @SpringBootApplication
 @EnableCaching
+@Slf4j
 public class DemoApplication {
 
 
@@ -20,5 +25,15 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Value("${app.name}")
+    String appName;
+    @Value("${env}")
+    String env;
+
+    @PostConstruct
+    public void yeld(){
+        log.info("env: {}, application name : {}",env,appName);
     }
 }
